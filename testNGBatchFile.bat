38179087ad2d3d4fb6c1b125774a3dc142f315da
rivi <rivi@rivi-PC.int.lemontree.se>
2017-03-22 16:09:05 +0100

    Removed xml and bat

--- testNGBatchFile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\Rivi\workspace\com.learnautomation.cucumber
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
