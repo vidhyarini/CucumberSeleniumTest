eb4e7f43c0c8cd2108f9cd84c7fe490335ac5e97
rivi <rivi@rivi-PC.int.lemontree.se>
2017-03-23 09:41:40 +0100

    Removed tag = @SmokeTest

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Features",glue ={"stepDefinition"},tags="@SmokeTest",plugin = "html:target/cucumber-html-report")
+@CucumberOptions(features = "Features",glue ={"stepDefinition"},plugin = "html:target/cucumber-html-report")
 public class TestRunner {
 
 }
